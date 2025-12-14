d96a8178d4b0f76c4c3fd10af4999236cbb1c69c
Jasper Smet <josbeir@gmail.com>
2025-12-14 16:15:01 +0100

    Adjust @since on new plugin class

--- src/QueuePlugin.php
+++ src/QueuePlugin.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org/)
  * @link          https://cakephp.org CakePHP(tm) Project
- * @since         0.1.0
+ * @since         2.2.1
  * @license       https://opensource.org/licenses/MIT MIT License
  */
 namespace Cake\Queue;
